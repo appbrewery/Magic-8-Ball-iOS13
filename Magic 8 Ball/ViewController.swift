14c03e8c54653c190bcefc943ef857f423cfaa2f
wjn <blitzes.toll.0n@icloud.com>
2022-04-28 07:38:00 -0500

    replaced viewDidLoad() override

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -12,7 +12,10 @@ class ViewController: UIViewController {
     
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
 
-
+    override func viewDidLoad() {
+       super.viewDidLoad()
+       // Do any additional setup after loading the view.
+    }
 
 }
 
